366f0e0601c47510cbedfcbeb00839f99d3611b7
Florian <florian.tousch@hotmail.fr>
2022-01-04 23:23:42 +0100

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Florian on 04/01/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
