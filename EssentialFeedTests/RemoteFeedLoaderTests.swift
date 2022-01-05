a7efdd4f1af00501e3b2bc2e2676eeb8051b5885
Florian <florian.tousch@hotmail.fr>
2022-01-05 09:41:19 +0100

    Add `test_init_doesNotRequestDataFromURL`

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Florian on 04/01/2022.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
