cac259461196b37ba8911ca6e8b97a97f3ccecde
FlorianTo <florian.tousch@hotmail.fr>
2022-01-14 12:22:48 +0100

    Extract duplicated test helper into a shared scope

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -186,16 +186,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         override func stopLoading() {}
     }
     
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
     private func anyData() -> Data {
         return Data("any data".utf8)
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
     }
     
     private func nonHTTPURLResponse() -> URLResponse {
